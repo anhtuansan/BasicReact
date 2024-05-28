500a969b0fbc09af0a282210144bee0afd09cb1c
AnhTuanSan <tuanvahe2021@gmail.com>
2024-05-28 22:00:51 +0700

    B5

--- src/components/learnBootstrap/HomePage/HomePage.jsx
+++ src/components/learnBootstrap/HomePage/HomePage.jsx
@@ -6,6 +6,57 @@ import "./HomePage.css";
 import Image from "react-bootstrap/Image";
 import exampleImage from "../../../assets/images/avatar.jpg";
 
+import {
+  MDBContainer as Container2,
+  MDBRow as Row,
+  MDBCol as Col,
+  MDBCard as Card,
+  MDBCardBody as CardBody,
+  MDBCardTitle as CardTitle,
+  MDBCardText as CardText,
+  MDBCardImage as CardImage,
+  MDBBtn as Btn,
+} from "mdb-react-ui-kit";
+
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    description: "Description for product 1",
+    image: exampleImage,
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    description: "Description for product 2",
+    image: exampleImage,
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    description: "Description for product 3",
+    image: exampleImage,
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    description: "Description for product 4",
+    image: exampleImage,
+  },
+  {
+    id: 5,
+    name: "Product 5",
+    description: "Description for product 5",
+    image: exampleImage,
+  },
+  {
+    id: 6,
+    name: "Product 6",
+    description: "Description for product 6",
+    image: exampleImage,
+  },
+];
+
 function HomePage() {
   const seasons = ["Spring", "Summer", "Autumn", "Winter"];
 
@@ -55,26 +106,28 @@ function HomePage() {
       {/* </div> */}
 
       <div id="listItem">
-        {seasons.map((season) => (
-          <div className="card" style={{ width: 300 }}>
-            <img
-              className="card-img-top"
-              src={exampleImage}
-              alt="Card image"
-              style={{ width: "100%" }}
-            />
-            <div className="card-body">
-              <h4 className="card-title">John Doe</h4>
-              <p className="card-text">
-                Some example text some example text. John Doe is an architect
-                and engineer
-              </p>
-              <a href="#" className="btn btn-primary stretched-link">
-                See Profile
-              </a>
-            </div>
-          </div>
-        ))}
+        <Container>
+          <Row>
+            {products.map((product, index) => (
+              <Col key={product.id} sm="12" md="6" lg="3" className="mb-4">
+                <Card>
+                  <CardImage
+                    src={product.image}
+                    alt={product.name}
+                    top
+                    hover
+                    overlay="white-slight"
+                  />
+                  <CardBody>
+                    <CardTitle>{product.name}</CardTitle>
+                    <CardText>{product.description}</CardText>
+                    <Btn color="info">Detail</Btn>
+                  </CardBody>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </div>
 
       {/* Footer */}
