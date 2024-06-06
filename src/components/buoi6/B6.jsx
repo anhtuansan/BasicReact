4a7a8c9a918de9ab53b33fb39c189137a11eeab3
AnhTuanSan <tuanvahe2021@gmail.com>
2024-06-06 21:24:05 +0700

    B7

--- src/components/buoi6/B6.jsx
+++ src/components/buoi6/B6.jsx
@@ -1,6 +1,11 @@
 // import "./Nav.css";
 
+import { useNavigate } from "react-router-dom";
+
 function B6() {
+  const navigate = useNavigate();
+
+  //navigate("/about");
   return (
     <>
       <h2>B6</h2>
---
+++ src/components/buoi6/Test.jsx
@@ -0,0 +1,10 @@
+// import "./Nav.css";
+
+function Test() {
+  return (
+    <>
+      <h2>Test</h2>
+    </>
+  );
+}
+export default Test;
