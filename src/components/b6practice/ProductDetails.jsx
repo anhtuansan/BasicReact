47c0586711001a0c5e7d4a14bb15915cb6700897
gloomyP1 <hoangphuc29290606@gmail.com>
2024-06-13 22:11:39 +0700

    update

--- src/components/b6practice/ProductDetails.jsx
+++ src/components/b6practice/ProductDetails.jsx
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import {
   MDBCard as Card,
   MDBCardBody as CardBody,
@@ -9,49 +12,59 @@ import {
 } from "mdb-react-ui-kit";
 import exampleImage from "../../assets/images/avatar.jpg";
 
-const products = [
-  {
-    id: 1,
-    name: "Product 1",
-    description: "Description for product 1",
-    image: exampleImage,
-  },
-  {
-    id: 2,
-    name: "Product 2",
-    description: "Description for product 2",
-    image: exampleImage,
-  },
-  {
-    id: 3,
-    name: "Product 3",
-    description: "Description for product 3",
-    image: exampleImage,
-  },
-  {
-    id: 4,
-    name: "Product 4",
-    description: "Description for product 4",
-    image: exampleImage,
-  },
-  {
-    id: 5,
-    name: "Product 5",
-    description: "Description for product 5",
-    image: exampleImage,
-  },
-  {
-    id: 6,
-    name: "Product 6",
-    description: "Description for product 6",
-    image: exampleImage,
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     name: "Product 1",
+//     description: "Description for product 1",
+//     image: exampleImage,
+//   },
+//   {
+//     id: 2,
+//     name: "Product 2",
+//     description: "Description for product 2",
+//     image: exampleImage,
+//   },
+//   {
+//     id: 3,
+//     name: "Product 3",
+//     description: "Description for product 3",
+//     image: exampleImage,
+//   },
+//   {
+//     id: 4,
+//     name: "Product 4",
+//     description: "Description for product 4",
+//     image: exampleImage,
+//   },
+//   {
+//     id: 5,
+//     name: "Product 5",
+//     description: "Description for product 5",
+//     image: exampleImage,
+//   },
+//   {
+//     id: 6,
+//     name: "Product 6",
+//     description: "Description for product 6",
+//     image: exampleImage,
+//   },
+// ];
 
 const ProductDetail = () => {
   const { id } = useParams();
-  const product = products.find((product) => product.id == id); 
-
+  // const product = products.find((product) => product.id == id); 
+  const [product, setProduct] = useState([]);
+useEffect(()=>{
+  axios 
+  .get(`https://63fc248e677c415873068d86.mockapi.io/product/${id}`)
+  .then((response) =>{
+    setProduct(response.data)
+    })
+    .catch((error) => {
+      console.error("There was an error fetching users!", error);
+    });
+    }, []);
   return (
     <Container>
       {product ? (
--- src/components/buoi5/List.jsx
+++ src/components/buoi5/List.jsx
@@ -1,7 +1,8 @@
 import Container from "react-bootstrap/Container";
 import exampleImage from "../../assets/images/avatar.jpg";
 import { useNavigate } from "react-router-dom";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 import {
   MDBContainer as Container2,
   MDBRow as Row,
@@ -13,45 +14,59 @@ import {
   MDBCardImage as CardImage,
   MDBBtn as Btn,
 } from "mdb-react-ui-kit";
-const products = [
-  {
-    id: 1,
-    name: "Product 1",
-    description: "Description for product 1",
-    image: exampleImage,
-  },
-  {
-    id: 2,
-    name: "Product 2",
-    description: "Description for product 2",
-    image: exampleImage,
-  },
-  {
-    id: 3,
-    name: "Product 3",
-    description: "Description for product 3",
-    image: exampleImage,
-  },
-  {
-    id: 4,
-    name: "Product 4",
-    description: "Description for product 4",
-    image: exampleImage,
-  },
-  {
-    id: 5,
-    name: "Product 5",
-    description: "Description for product 5",
-    image: exampleImage,
-  },
-  {
-    id: 6,
-    name: "Product 6",
-    description: "Description for product 6",
-    image: exampleImage,
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     name: "Product 1",
+//     description: "Description for product 1",
+//     image: exampleImage,
+//   },
+//   {
+//     id: 2,
+//     name: "Product 2",
+//     description: "Description for product 2",
+//     image: exampleImage,
+//   },
+//   {
+//     id: 3,
+//     name: "Product 3",
+//     description: "Description for product 3",
+//     image: exampleImage,
+//   },
+//   {
+//     id: 4,
+//     name: "Product 4",
+//     description: "Description for product 4",
+//     image: exampleImage,
+//   },
+//   {
+//     id: 5,
+//     name: "Product 5",
+//     description: "Description for product 5",
+//     image: exampleImage,
+//   },
+//   {
+//     id: 6,
+//     name: "Product 6",
+//     description: "Description for product 6",
+//     image: exampleImage,
+//   },
+// ];
+
+
 function List() {
+  const [products, setProducts] = useState([]);
+
+useEffect (()=>{
+ axios
+.get("https://63fc248e677c415873068d86.mockapi.io/product")
+.then((response) =>{
+setProducts(response.data)
+})
+.catch((error) => {
+  console.error("There was an error fetching users!", error);
+});
+}, []);
   const navigate = useNavigate();
 
   function chuyenTrang(productId) {
